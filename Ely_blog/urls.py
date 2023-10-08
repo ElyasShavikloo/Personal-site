f5018fb72bffa5ef437c95fdeea9baedebf9baf3
Elyas Shavikloo <shaviklooe@gmail.com>
2023-10-08 10:42:10 +0330

    level 2, doing changes: "create home(app), add index.html template and changes it , add assets folder and set it and so create view and urls(2) "

--- Ely_blog/urls.py
+++ Ely_blog/urls.py
@@ -21,4 +21,5 @@ from Ely_blog import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
